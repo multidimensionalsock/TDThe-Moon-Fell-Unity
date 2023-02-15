048bf74e76c70e7e0de214a1df523c8e9ec96ae5
voidrobyn <92083124+AleneVedMidnat@users.noreply.github.com>
2023-02-15 20:18:45 +0000

    .

--- The day the moon fell/Assets/Character/Scripts/PlayerMovement.cs
+++ The day the moon fell/Assets/Character/Scripts/PlayerMovement.cs
@@ -11,19 +11,25 @@ public class PlayerMovement : MonoBehaviour
 {
 	private PlayerInput m_Input;
 	private Rigidbody2D m_Rigidbody;
+	private Vector2 m_Movement;
 	void Awake()
 	{
 		m_Input = GetComponent<PlayerInput>();
 		m_Rigidbody = GetComponent<Rigidbody2D>();
 		m_Input.currentActionMap.FindAction("Movement").performed += Handle_Movement_Performed;
-		m_Input.currentActionMap.FindAction("Jump").performed += Handle_Jump_Performed;
-		m_Input.currentActionMap.FindAction("Movement").canceled += Handle_Movement_Cancelled;
-		m_Input.currentActionMap.FindAction("Jump").canceled += Handle_Jump_Cancelled;
+		//m_Input.currentActionMap.FindAction("Jump").performed += Handle_Jump_Performed;
+		//m_Input.currentActionMap.FindAction("Movement").canceled += Handle_Movement_Cancelled;
 	}
 
-	// Update is called once per frame
-	void Update()
-    {
-        
-    }
+	#region movementHandling
+	void Handle_Movement_Performed(InputAction.CallbackContext context)
+	{
+		m_Movement = context.ReadValue<Vector2>();
+		//movement coroutine 
+	}
+	#endregion
+
+	#region movement 
+
+	#endregion
 }
