32fdab49a5494966811528b65a0822c369805d24
voidrobyn <92083124+AleneVedMidnat@users.noreply.github.com>
2023-02-16 20:53:29 +0000

    Animations made

--- The day the moon fell/Assets/Character/Scripts/PlayerMovement.cs
+++ The day the moon fell/Assets/Character/Scripts/PlayerMovement.cs
@@ -12,24 +12,44 @@ public class PlayerMovement : MonoBehaviour
 	private PlayerInput m_Input;
 	private Rigidbody2D m_Rigidbody;
 	private Vector2 m_Movement;
+	private Coroutine m_moveCoroutine;
+
+	[Header("Movement variables")]
+	[SerializeField] float m_movementSpeed;
 	void Awake()
 	{
 		m_Input = GetComponent<PlayerInput>();
 		m_Rigidbody = GetComponent<Rigidbody2D>();
-		m_Input.currentActionMap.FindAction("Movement").performed += Handle_Movement_Performed;
-		//m_Input.currentActionMap.FindAction("Jump").performed += Handle_Jump_Performed;
-		//m_Input.currentActionMap.FindAction("Movement").canceled += Handle_Movement_Cancelled;
+		m_Input.currentActionMap.FindAction("Movement").performed += MoveStart;
+		//m_Input.currentActionMap.FindAction("Jump").performed += Jump;
+		m_Input.currentActionMap.FindAction("Movement").canceled += MoveEnd;
 	}
 
 	#region movementHandling
-	void Handle_Movement_Performed(InputAction.CallbackContext context)
+	void MoveStart(InputAction.CallbackContext context)
 	{
 		m_Movement = context.ReadValue<Vector2>();
-		//movement coroutine 
+		m_moveCoroutine = StartCoroutine(Move());
 	}
+
+	void MoveEnd(InputAction.CallbackContext context)
+	{
+		m_Movement = Vector2.zero;
+		StopCoroutine(m_moveCoroutine);
+	}
+
 	#endregion
 
 	#region movement 
+	IEnumerator Move()
+	{
+		while (m_Movement != Vector2.zero)
+		{
+			m_Rigidbody.AddForce(m_Movement * m_movementSpeed * Time.fixedDeltaTime, ForceMode2D.Impulse);
+
+			yield return new WaitForFixedUpdate();
+		}
+	}
 
 	#endregion
 }
