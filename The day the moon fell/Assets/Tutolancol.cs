669783e62b158c84a7f78fb391d212dd87d9d2dd
voidrobyn <92083124+AleneVedMidnat@users.noreply.github.com>
2023-05-05 21:09:40 +0100

    press 1

---
+++ The day the moon fell/Assets/Tutolancol.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tutolancol : MonoBehaviour
+{
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		transform.GetChild(0).gameObject.SetActive(true);
+	}
+
+	private void OnTriggerExit2D(Collider2D collision)
+	{
+		transform.GetChild(0).gameObject.SetActive(false);
+	}
+}
